fe15397cb4ed6cdb25d080f6a9ce5f4fcdab4906
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-03 13:01:06 -0500

    Transpilation

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 4500;
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.get("/test", (res) => {
     res.send("Hello World");
 });
