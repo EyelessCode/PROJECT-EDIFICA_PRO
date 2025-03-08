718a808977c66c468c8d9baae6f9fe7d3c226aff
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-08 17:33:24 -0500

    Update description.attribute.ts

---
+++ src/task/domain/validation/task/description.attribute.ts
@@ -0,0 +1,15 @@
+export class TaskDescription{
+    value:string
+
+    constructor(value:string){
+        this.value = value;
+        this.descriptionValidation()
+    }
+
+    private descriptionValidation(){
+        let word=this.value.split(" ")
+        if(word.length > 100){
+            throw new Error("Description must be at least 100 words long");
+        }
+    }
+}
