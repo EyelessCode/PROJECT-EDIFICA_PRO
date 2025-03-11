cda6a9dd38690b8aeca41c6d823c72e0c471cd7e
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-10 23:34:04 -0500

    Update createTask.service.ts

--- src/task/app/service/createTask.service.ts
+++ src/task/app/service/createTask.service.ts
@@ -1,12 +1,19 @@
 import { IServiceTask } from "../../domain/interface/repository/task.interface.service";
 import { TaskModel } from "../../domain/model/task.model";
+import { TaskDeadline } from "../../domain/validation/deadline.attribute";
+import { TaskDescription } from "../../domain/validation/description.attribute";
+import { TaskId } from "../../domain/validation/id.attribute";
+import { TaskTitle } from "../../domain/validation/title.attribute";
 
-export class CreateServiceTask{
-    constructor(private readonly service:IServiceTask) {
-        
-    }
+export class CreateServiceTask {
+    constructor(private service: IServiceTask) {}
 
-    async create(task:TaskModel):Promise<void>{
+    async create(id: number, title: string, description: string,
+        deadline: Date
+    ): Promise<void> {
+        const task = new TaskModel(new TaskId(id),
+            new TaskTitle(title), new TaskDescription(description),
+            new TaskDeadline(deadline))
         return await this.service.createTask(task)
     }
 }
