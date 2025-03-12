4f9a2e4a93c8b9da448a57ec171662860ab77da1
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-12 17:50:34 -0500

    Update getTask.service.ts

--- src/task/app/service/getTask.service.ts
+++ src/task/app/service/getTask.service.ts
@@ -1,11 +1,18 @@
 import { IServiceTask } from "../../domain/interface/repository/task.interface.service";
 import { TaskModel } from "../../domain/model/task.model";
+import { HandleError } from "../../domain/util/handleError.util";
 import { TaskId } from "../../domain/validation/id.attribute";
 
 export class GetOneServiceTask {
     constructor(private readonly service: IServiceTask) { }
 
-    async getOneTask(id: number): Promise<TaskModel | null> {
-        return await this.service.getTask(new TaskId(id));
+    async getOneTask(id: number): Promise<TaskModel> {
+        const task = await this.service.getTask(new TaskId(id));
+
+        if (!task) {
+            throw new HandleError(`Task not found!`);
+        }
+
+        return task;
     }
 }
