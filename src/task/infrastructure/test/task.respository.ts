545ad15f7f90810fa84a50a78fa2c04528643a03
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-12 19:20:02 -0500

    Update task.respository.ts

--- src/task/infrastructure/test/task.respository.ts
+++ src/task/infrastructure/test/task.respository.ts
@@ -3,20 +3,33 @@ import { TaskModel } from "../../domain/model/task.model";
 import { TaskId } from "../../domain/validation/id.attribute";
 
 export class TaskRepository implements IServiceTask {
+    //? This repository is in memory activity. It's just a test
+    private tasks: TaskModel[] = []
+
     async createTask(Task: TaskModel): Promise<void> {
-        throw new Error("Method not implemented.");
+        //// throw new Error("Method not implemented.");
+        this.tasks.push(Task)
     }
+
     async getTask(id: TaskId): Promise<TaskModel | null> {
-        throw new Error("Method not implemented.");
+        //// throw new Error("Method not implemented.");
+        return this.tasks.find((taskFound) => taskFound.id.value === id.value) || null
     }
+
     async getAll(): Promise<TaskModel[]> {
-        throw new Error("Method not implemented.");
+        //// throw new Error("Method not implemented.");
+        return this.tasks
     }
+
     async updateTask(task: TaskModel): Promise<void> {
-        throw new Error("Method not implemented.");
+        //// throw new Error("Method not implemented.");
+        const index = this.tasks.findIndex((taskFound) => taskFound.id.value === task.id.value)
+        this.tasks[index] = task
     }
+
     async deleteTask(id: TaskId): Promise<void> {
-        throw new Error("Method not implemented.");
+        //// throw new Error("Method not implemented.");
+        this.tasks.filter((taskFound) => taskFound.id.value !== id.value)
     }
 
 }
