8798ce619b42c248b18824bb3936a37323ac08d0
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 17:03:06 -0500

    Update getTask.repository.ts

--- src/task/infrastructure/repository/getTask.repository.ts
+++ src/task/infrastructure/repository/getTask.repository.ts
@@ -2,22 +2,27 @@ import { PrismaClient } from "@prisma/client";
 import { TaskId } from "../../domain/validation/id.attribute";
 import { TaskModel } from "../../domain/model/task.model";
 import { TaskMapper } from "./mapper/taskModel.mapper";
+import { TaskNotCreated, TaskNotFoundError } from '../../domain/util/handleError.util';
 
 export class GetOneRepositoryTask{
     constructor(private prisma:PrismaClient){}
 
     async getTask(id: TaskId): Promise<TaskModel | null> {
         //// throw new Error("Method not implemented.");
-        const task= await this.prisma.task.findUnique({
-            where:{
-                id:id.value
+        try {
+            const task= await this.prisma.task.findUnique({
+                where:{
+                    id:id.value
+                }
+            })
+    
+            if (!task) {
+                return null
             }
-        })
-
-        if (!task) {
-            return null
+    
+            return TaskMapper.toDomain(task);
+        } catch (error) {
+            throw new TaskNotCreated(`Task with id ${id.value} not found`);
         }
-
-        return TaskMapper.toDomain(task);
     }
 }
