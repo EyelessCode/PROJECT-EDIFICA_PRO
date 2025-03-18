006a28c5f9a8437b89b834337c11404593c637b6
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 11:51:19 -0500

    Update taskModel.mapper.ts

---
+++ src/task/infrastructure/repository/mapper/taskModel.mapper.ts
@@ -0,0 +1,28 @@
+import { TaskModel } from "../../../domain/model/task.model";
+import { TaskDeadline } from "../../../domain/validation/deadline.attribute";
+import { TaskDescription } from "../../../domain/validation/description.attribute";
+import { TaskId } from "../../../domain/validation/id.attribute";
+import { TaskState } from "../../../domain/validation/state.attribute";
+import { TaskTitle } from "../../../domain/validation/title.attribute";
+
+export class TaskMapper {
+    static toDomain(task: any): TaskModel {
+        return new TaskModel(
+            new TaskId(task.id),
+            new TaskTitle(task.title),
+            new TaskDescription(task.description),
+            new TaskState(task.state),
+            new TaskDeadline(task.deadline)
+        );
+    }
+
+    static toPersistence(task: TaskModel): any {
+        return {
+            id: task.id.value,
+            title: task.title.value,
+            description: task.description.value,
+            state: task.state.value,
+            deadline: task.deadline.value
+        };
+    }
+}
