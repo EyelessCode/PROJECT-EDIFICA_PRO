d50570871a78a0b33858d51480642932a437c341
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 18:59:18 -0500

    Update getAllTask.repository.ts

---
+++ src/task/infrastructure/repository/getAllTask.repository.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+import { TaskModel } from "../../domain/model/task.model";
+import { TaskNotFoundError } from "../../domain/util/handleError.util";
+import { TaskMapper } from "./mapper/taskModel.mapper";
+
+export class GetAllRepositoryTasks{
+    constructor(private prisma:PrismaClient){}
+
+    async getAllTasks():Promise<TaskModel[]>{
+        try {
+            const tasks=await this.prisma.task.findMany()
+            return tasks.map((task)=>TaskMapper.toDomain(task))
+        } catch (error) {
+            throw new TaskNotFoundError(`Tasks not found!`)
+        }
+    }
+}
