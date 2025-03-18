554f809dd60394e00ffd5e5b671c8361bacff618
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 10:36:26 -0500

    Testing something...

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const PORT = process.env.PORT || 4500;
+const prisma=new PrismaClient({
+    log:["info","warn","error"]
+})
 
 app.use(express.json())
 app.use(cors())
@@ -14,3 +18,5 @@ app.get('/test', (req: Request, res: Response) => {
 })
 
 app.listen(PORT, () => console.log(`LISTENING IN ${PORT} PORT!`))
+
+export {prisma}
