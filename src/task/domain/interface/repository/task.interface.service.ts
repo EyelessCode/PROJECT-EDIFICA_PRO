ae0d1a41d513c698e7215e8f4461504dd7d3a52c
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 18:21:24 -0500

    Update task.interface.service.ts

--- src/task/domain/interface/repository/task.interface.service.ts
+++ src/task/domain/interface/repository/task.interface.service.ts
@@ -2,7 +2,7 @@ import { TaskModel } from "../../model/task.model";
 import { TaskId } from "../../validation/id.attribute";
 
 export interface IServiceTask {
-    createTask(Task: TaskModel): Promise<void>
+    createTask(task: TaskModel): Promise<void>
     getTask(id: TaskId): Promise<TaskModel | null>
     getAll(): Promise<TaskModel[]>
     updateTask(task: TaskModel): Promise<void>
