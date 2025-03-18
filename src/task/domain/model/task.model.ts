b27ccf04e726c76fcf846d088dee3b0cf2d1c066
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 11:51:09 -0500

    Update task.model.ts

--- src/task/domain/model/task.model.ts
+++ src/task/domain/model/task.model.ts
@@ -1,29 +1,32 @@
 import { TaskDeadline } from "../validation/deadline.attribute";
 import { TaskDescription } from "../validation/description.attribute";
 import { TaskId } from "../validation/id.attribute";
+import { TaskState } from "../validation/state.attribute";
 import { TaskTitle } from "../validation/title.attribute";
 
 export class TaskModel {
     id: TaskId;
     title: TaskTitle;
     description: TaskDescription;
+    state:TaskState
     deadline: TaskDeadline;
 
     constructor(id: TaskId, title: TaskTitle, description: TaskDescription,
-        deadline: TaskDeadline
+        state:TaskState, deadline: TaskDeadline
     ) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.deadline = deadline;
+        this.state=state
     }
 
     public toString() {
         return `Task ID: ${this.id}, Title: ${this.title}, Description: ${this
-            .description}, Deadline: ${this.deadline}`
+            .description}, State: ${this.state}, Deadline: ${this.deadline}`
     }
 
     public titlePlusDeadline() {
-        return `Title: ${this.title} and its time will end: ${this.deadline}`
+        return `Title: ${this.title} - State: ${this.state} and its time will end: ${this.deadline}`
     }
 }
