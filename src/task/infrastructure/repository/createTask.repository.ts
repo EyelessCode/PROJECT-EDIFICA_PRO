5da5ca4ed0422a38b157053b4a0d8805fe93ee8d
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-18 18:27:55 -0500

    Update createTask.repository.ts

--- src/task/infrastructure/repository/createTask.repository.ts
+++ src/task/infrastructure/repository/createTask.repository.ts
@@ -1,26 +1,22 @@
 import { PrismaClient } from "@prisma/client";
 import { TaskModel } from "../../domain/model/task.model";
 import { TaskNotCreated } from "../../domain/util/handleError.util";
+import { TaskMapper } from "./mapper/taskModel.mapper";
 
 export class CreateRepositoryTask{
     constructor(private prisma:PrismaClient){}
 
-    async createTask(task: TaskModel): Promise<void> {
+    async createTask(task: TaskModel): Promise<TaskModel> {
         //// throw new Error("Method not implemented.");
         try {
-            const object={
-                title:task.title.value,
-                description:task.description.value,
-                state:task.state.value,
-                deadline:task.deadline.value
-            }
-
+            const object=TaskMapper.toPersistence(task)
             const taskToCreate=await this.prisma.task.create({
                 data:object
                 });
+
+                return TaskMapper.toDomain(taskToCreate)
         } catch (error) {
             throw new TaskNotCreated(`Error creating task`);
         }
     }
-
 }
