f94f32990d4fcaefdca5612f001601c254ede4ca
Cristhian Guedes <cristhianjosue03@outlook.com>
2025-03-22 19:34:21 -0500

    Update handleError.util.ts

--- src/task/domain/util/handleError.util.ts
+++ src/task/domain/util/handleError.util.ts
@@ -1,15 +1,21 @@
 export class TaskNotFoundError extends Error{
+    statusCode:number
+    
     constructor(message:string){
         super(message)
         this.name="TaskNotFoundError"
+        this.statusCode=404
         Object.setPrototypeOf(this,TaskNotFoundError.prototype)
     }
 }
 
 export class TaskNotCreated extends Error{
+    statusCode:number
+
     constructor(message:string){
         super(message)
         this.name="TaskNotCreated"
+        this.statusCode=404
         Object.setPrototypeOf(this,TaskNotCreated.prototype)
     }
 }
